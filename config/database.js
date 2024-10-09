966f49c0517158ff031541d7d2d580ef6a40dc72
Dummy <onyourleftpal@gmail.com>
2024-10-09 20:02:40 +0300

    1.0 our website

--- config/database.js
+++ config/database.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
-
+const url = `mongodb+srv://sarah:sarah@cluster0.xr5qdwo.mongodb.net/name`
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/yourDatabase', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(url);
         console.log('MongoDB connected');
     } catch (err) {
         console.error(err);
